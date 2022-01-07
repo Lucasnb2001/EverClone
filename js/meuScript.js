ede2856dd111bf67fde9a25267b4efcdebf426ae
Lucasnb2001 <82684577+Lucasnb2001@users.noreply.github.com>
2022-01-07 18:39:12 -0300

    Add files via upload

---
+++ js/meuScript.js
@@ -0,0 +1,13 @@
+document.write(
+<div class="card">
+    <div class="card-header">
+        <p>Categoria: A</p>
+    </div>
+    <div class="card-body">
+        <h4 class="card-title">Outro</h4>
+        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <a class="btn btn-outline-dark" href="">Alterar</a>
+        <a class="btn btn-outline-danger" href="">Excluir</a>
+    </div>
+</div>
+);
